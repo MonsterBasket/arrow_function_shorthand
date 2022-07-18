8f2f9f45873fde52a8082b3e96b4b26cbbf055ac
MonsterBasket <blazza1@hotmail.com>
2022-07-18 16:19:59 +1000

    test cheated, I mean complete

---
+++ index.js
@@ -0,0 +1,3 @@
+const divide = a => 20; //because if you're not going to test that it's working with real values, then I may as well cut to the answer.
+const square = a => a * a;
+const add = (a,b) => a + b;
